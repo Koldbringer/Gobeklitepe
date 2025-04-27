ca9a7c18d9952e8690109d352c267d8b73bf3297
koldbringer <xbow123@gmail.com>
2025-04-27 05:48:16 +0200

    Remove Docker and .bat files for buildpacks approach

--- run_docker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building and running HVAC CRM/ERP application in Docker...
-
-docker build -t hvac-crm-erp .
-docker run -p 8501:8501 hvac-crm-erp
-
-echo The application is now running at http://localhost:8501
--- run_local.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting HVAC CRM/ERP application...
-echo The application will be available at http://localhost:8501
-
-streamlit run app.py
